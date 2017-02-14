028485abcced0695edecf49ed10d4bbd7d4c786b
ftumj259 <2013470259@ftumj.ac.id>
2017-02-14 07:28:11 +0700

    Register OK

--- djauth/views.py
+++ djauth/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 from django.template import RequestContext
-from django.contrib.auth import *
+from django.contrib.auth.models import *
 from django.contrib import auth
 
 def home(request):
