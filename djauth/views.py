502864bb5c8e2f3cbe72d9777b8bdbc8fc9c58ea
ftumj259 <2013470259@ftumj.ac.id>
2017-02-22 11:14:57 +0700

    dashboard and frontpages web

--- djauth/views.py
+++ djauth/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
-from .models import About, Post, Komentar, Gambar
+from .models import About, Komentar, Gambar
 
 from django.template import RequestContext
 from django.contrib.auth.models import *
@@ -26,7 +26,7 @@ def login(request):
 @login_required
 def dashboard(request):
     if request.user.is_authenticated():
-        return render_to_response('dashboard.html', locals(), context_instance=RequestContext(request))
+        return render_to_response('admin/dashboard.html', locals(), context_instance=RequestContext(request))
     return HttpResponse('Hanya bisa di askses oleh User')
 
 def logout(request):
@@ -52,8 +52,13 @@ def register(request):
     return render_to_response('register.html', locals(), context_instance=RequestContext(request))
 
 def about(request):
-    about = About.objects.get(id=1)
-    return render_to_response('about.html', locals(), context_instance=RequestContext(request))
+    try:
+        about = About.objects.get(id=1)
+        return render_to_response('about.html', locals(), context_instance=RequestContext(request))
+    except Exception, err:
+        about = False
+        return render_to_response('about.html', locals(), context_instance=RequestContext(request))
+
 
 def about_detail(request):
     about = About.objects.get(id=1)
@@ -78,8 +83,11 @@ def about_komentar(request):
     return HttpResponse('Hanya bisa di askses oleh User')
 
 def portfolio(request):
+    gambar = Gambar.objects.all()
+    # komentator = Komentar.objects.filter(untuk='komentar')
     return render_to_response('portfolio.html', locals(), context_instance=RequestContext(request))
 
+@login_required
 def upload_gambar(request):
     if request.POST:
         nama = request.POST.get('nama')
@@ -89,8 +97,8 @@ def upload_gambar(request):
         image_result = open('media/image_upload/%s' % gambar,'wb')
         image_result.write(gambar.read())
 
-        return render_to_response('portfolio.html', locals(), context_instance=RequestContext(request))
-    return render_to_response('upload_gambar.html', locals(), context_instance=RequestContext(request))
+        return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
 
 
 
