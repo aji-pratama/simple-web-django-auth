8ee02270731a863505cff394b10d07236750d24d
ftumj259 <2013470259@ftumj.ac.id>
2017-02-24 07:06:58 +0700

    Finally Done

--- djauth/views.py
+++ djauth/views.py
@@ -1,15 +1,79 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from .models import About, Komentar, Gambar
+from django.utils import timezone
 
 from django.template import RequestContext
 from django.contrib.auth.models import *
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-def index(request):
-    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
+# JUST FOR ADMIN
+@user_passes_test(lambda u: u.is_superuser)
+def dashboard(request):
+    if request.user.is_authenticated():
+        return render_to_response('admin/dashboard.html', locals(), context_instance=RequestContext(request))
+    return HttpResponse('Hanya bisa di askses oleh User')
 
+@user_passes_test(lambda u: u.is_superuser)
+def about_edit(request):
+    about = About.objects.get(id=1)
+    if request.POST:
+        about.title = request.POST.get('title')
+        about.desc = request.POST.get('desc')
+        about.desc_detail = request.POST.get('desc_detail')
+        about.tgl_post = timezone.now()
+        about.save()
+        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('admin/edit_about.html', locals(), context_instance=RequestContext(request))
+
+@user_passes_test(lambda u: u.is_superuser)
+def upload_gambar(request):
+    gambar = Gambar.objects.all().order_by('-id')
+    if request.POST:
+        nama = request.POST.get('nama')
+        gambars = request.FILES.get('gambar')
+        ga = Gambar(nama=nama, gambar=gambars)
+        ga.save()
+        image_result = open('media/image_upload/%s' % gambars,'wb')
+        image_result.write(gambars.read())
+
+        gambar = Gambar.objects.all()
+        return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
+
+@user_passes_test(lambda u: u.is_superuser)
+def about_admin(request):
+    try:
+        about = About.objects.get(id=1)
+        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
+    except Exception, err:
+        about = False
+        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
+
+# FOR MEMBER LOGGED IN NON ADMIN & ADMIN
+@user_passes_test(lambda u: u.is_active)
+def about_komentar(request):
+    about = About.objects.get(id=1)
+    komentar = Komentar.objects.filter(untuk='about')
+    if request.user.is_authenticated():
+        if request.POST:
+            body = request.POST.get('body')
+            komentator = request.POST.get('komentator')
+            untuk = request.POST.get('untuk')
+
+            k = Komentar(body=body, komentator=komentator, untuk=untuk)
+            k.save()
+
+            return render_to_response('about_detail.html', locals(), context_instance=RequestContext(request))
+    return HttpResponse('Hanya bisa di askses oleh User')
+
+@user_passes_test(lambda u: u.is_active)
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
+
+# FOR EVERYONE
 def login(request):
     if request.POST:
         username = request.POST.get('username')
@@ -20,18 +84,14 @@ def login(request):
         except Exception, err:
             return HttpResponse('Login Gagal')
         else:
-            return HttpResponseRedirect('/dashboard/')
+            if User.objects.filter(is_superuser=1):
+                return HttpResponseRedirect('/dashboard/')
+            else:
+                return HttpResponseRedirect('/')
     return render_to_response('login.html', locals(), context_instance=RequestContext(request))
 
-@login_required
-def dashboard(request):
-    if request.user.is_authenticated():
-        return render_to_response('admin/dashboard.html', locals(), context_instance=RequestContext(request))
-    return HttpResponse('Hanya bisa di askses oleh User')
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect('/')
+def index(request):
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def register(request):
     if request.POST:
@@ -59,66 +119,16 @@ def about(request):
         about = False
         return render_to_response('about.html', locals(), context_instance=RequestContext(request))
 
-
 def about_detail(request):
     about = About.objects.get(id=1)
     komentar = Komentar.objects.filter(untuk='about')
     return render_to_response('about_detail.html', locals(), context_instance=RequestContext(request))
-
-@login_required
-def about_komentar(request):
-    about = About.objects.get(id=1)
-    komentar = Komentar.objects.filter(untuk='about')
-    if request.user.is_authenticated():
-        if request.POST:
-            body = request.POST.get('body')
-            komentator = request.POST.get('komentator')
-            untuk = request.POST.get('untuk')
-
-            k = Komentar(body=body, komentator=komentator, untuk=untuk)
-            k.save()
-
-            return render_to_response('about_detail.html', locals(), context_instance=RequestContext(request))
-    return HttpResponse('Hanya bisa di askses oleh User')
 
 def portfolio(request):
     gambar = Gambar.objects.all()
     # komentator = Komentar.objects.filter(untuk='komentar')
     return render_to_response('portfolio.html', locals(), context_instance=RequestContext(request))
 
-@login_required
-def upload_gambar(request):
-    gambar = Gambar.objects.all().order_by('-id')
-    if request.POST:
-        nama = request.POST.get('nama')
-        gambars = request.FILES.get('gambar')
-        ga = Gambar(nama=nama, gambar=gambars)
-        ga.save()
-        image_result = open('media/image_upload/%s' % gambars,'wb')
-        image_result.write(gambars.read())
-
-        gambar = Gambar.objects.all()
-        return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
-    return render_to_response('admin/upload_gambar.html', locals(), context_instance=RequestContext(request))
-
-@login_required
-def about_admin(request):
-    try:
-        about = About.objects.get(id=1)
-        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
-    except Exception, err:
-        about = False
-        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
-
-def about_edit(request):
-    about = About.objects.get(id=1)
-    if request.POST:
-        about.title = request.POST.get('title')
-        about.desc = request.POST.get('desc')
-        about.desc_detail = request.POST.get('desc_detail')
-        about.save()
-        return render_to_response('admin/about.html', locals(), context_instance=RequestContext(request))
-    return render_to_response('admin/edit_about.html', locals(), context_instance=RequestContext(request))
 
 
 #
